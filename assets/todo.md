e108e14e87b3f41931cb4eeffd313695fd85bedb
Orio <maciejll@wp.pl>
2025-08-17 10:31:06 +0200

    Revert "0.711 Project-Task connection implementation schema"
    This reverts commit a8f0b764af721c46c210cc3f95328374d604a1fb.

---
+++ assets/todo.md
@@ -0,0 +1,6 @@
+Improve 'Add Project Dialog'
+Improve 'Add Task Dialog'
+Create 'Project Name Provider' for search
+Improve 'Add Task Dialog' with project search
+Create 'Project Card Screen'
+Add Task progress View to 'Project Card Screen'
--- lib/data/tables/block.dart
+++ lib/data/tables/block.dart
@@ -11,46 +11,16 @@ class Block extends Table {
   DateTimeColumn get dayLocal => dateTime()();
   IntColumn get startMinuteOfDay => integer()();
   IntColumn get lengthMinutes => integer()();
-  
-  // TODO: Add position_in_block column for multiple tasks per block support
-  // This column will be needed for the Add Block Dialog functionality
-  // to support multiple tasks within a single time block.
-  // 
-  // Add this column:
-  // IntColumn get positionInBlock => integer().withDefault(const Constant(0))();
-  // 
-  // This allows:
-  // - Multiple block entries for the same time slot
-  // - Tasks ordered by position (0, 1, 2, etc.)
-  // - Proper task sequencing within blocks
-  // - Support for task reordering
-  //
-  // After adding this column, you'll need to:
-  // 1. Update the database schema version in database.dart
-  // 2. Add migration logic to add this column to existing data
-  // 3. Update all queries that create/read blocks to handle position
-  // 4. Update BlockWithTask class to include position information
 }
 
 class BlockWithTask {
   final BlockData block;
   final String taskName;
-  // TODO: Add position field when positionInBlock column is added to Block table
-  // final int position;
 
-  BlockWithTask({
-    required this.block, 
-    required this.taskName,
-    // TODO: Add position parameter when implementing multiple tasks per block
-    // required this.position,
-  });
+  BlockWithTask({required this.block, required this.taskName});
 
   String get displayName => taskName.isEmpty ? "Unnamed task" : taskName;
 
-  // TODO: Update formatTimeRange for multiple tasks per block
-  // When multiple tasks exist in a block, this should show:
-  // - Individual task time slot within the block
-  // - Or overall block time range with task position indicator
   String formatTimeRange() {
     final startMinutes = block.startMinuteOfDay;
     final endMinutes = startMinutes + block.lengthMinutes;
@@ -59,10 +29,6 @@ class BlockWithTask {
     final endHour = (endMinutes ~/ 60).toString().padLeft(2, '0');
     final endMin = (endMinutes % 60).toString().padLeft(2, '0');
     return '$startHour:$startMin - $endHour:$endMin';
-    
-    // TODO: When position support is added, consider showing:
-    // - Task-specific time slot: "$startHour:$startMin - $endHour:$endMin (Task ${position + 1})"
-    // - Or block time with position: "$startHour:$startMin - $endHour:$endMin"
   }
 
   String formatDuration() {
@@ -79,11 +45,4 @@ class BlockWithTask {
       }
     }
   }
-
-  // TODO: Add methods for multiple task block support
-  // 
-  // bool get isPartOfMultiTaskBlock => ...
-  // int get totalTasksInBlock => ...
-  // String get positionLabel => "Task ${position + 1}";
-  // Duration get taskSpecificDuration => ...
 }
