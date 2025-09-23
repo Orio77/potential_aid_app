ea30bf5f665edc6fd12220ee46ad729aa26f4e60
Orio <maciejll@wp.pl>
2025-09-23 21:21:29 +0200

    0.82 Added Add Task Button To Block Edit Dialog

--- lib/widgets/edit_block_dialog.dart
+++ lib/widgets/edit_block_dialog.dart
@@ -6,6 +6,7 @@ import 'package:potential_aid_app/providers/block_with_tasks_notifier.dart';
 import 'package:potential_aid_app/providers/project_search_notifier.dart';
 import 'package:potential_aid_app/providers/projects_notifier.dart';
 import 'package:potential_aid_app/providers/schedule_notifier.dart';
+import 'package:potential_aid_app/widgets/add_task_dialog.dart';
 import 'package:potential_aid_app/widgets/duration_picker_dialog.dart';
 import 'package:potential_aid_app/widgets/schedule/block_add_task_list.dart';
 import 'package:potential_aid_app/widgets/util/search_text_field.dart';
@@ -176,9 +177,17 @@ class _EditTaskDialogState extends ConsumerState<EditBlockDialog> {
                 ),
               ),
 
-              const SizedBox(height: 16),
+              const SizedBox(height: 8),
 
               if (_selectedProject != null) ...[
+                ElevatedButton(
+                  onPressed: () =>
+                      showAddTaskDialog(context, _selectedProject!.id),
+                  child: Icon(Icons.add_task),
+                ),
+
+                const SizedBox(height: 8),
+
                 Expanded(
                   child: BlockAddTaskList(
                     key: ValueKey(_selectedProject?.id),
